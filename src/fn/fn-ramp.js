32571cc0745741ef1bc885ccccc95df5a35bc1ed
Raul Ochoa <rochoaf@gmail.com>
2016-02-23 20:22:43 +0100

    Allow to use a custom number of buckets in ramps
    Number of buckets can be specified with a param in numeric ramps
    and with array lenght in color ramps
    
    IMPORTANT: Backwards incompatible change
    Datasource now requires an extra param for the number of buckets.
    
    Closes #6

--- src/fn/fn-ramp.js
+++ src/fn/fn-ramp.js
@@ -7,13 +7,13 @@ var columnName = require('../helper/column-name');
 var postcss = require('postcss');
 
 module.exports = function (datasource, decl) {
-  return function fn$ramp (column, min, max, method) {
+  return function fn$ramp (column, /*...*/args) {
     debug('fn$ramp(%j)', arguments);
     debug('Using "%s" datasource to calculate ramp', datasource.getName());
 
-    method = method || 'quantiles';
+    args = Array.prototype.slice.call(arguments, 1);
 
-    return ramp(datasource, column, method, min, max)
+    return ramp(datasource, column, args)
       .then(function (rampResult) {
         var parent = decl.parent;
         var start = rampResult.shift();
@@ -25,7 +25,7 @@ module.exports = function (datasource, decl) {
 
         for (var i = 0; i < rampResult.length; i += 2) {
           var rule = postcss.rule({
-            selector: '[ ' + column + ' < ' + rampResult[i + 1] + ' ]'
+            selector: '[ ' + column + ' <= ' + rampResult[i + 1] + ' ]'
           });
           rule.append(postcss.decl({ prop: decl.prop, value: rampResult[i] }));
           parent.append(rule);
@@ -38,18 +38,64 @@ module.exports = function (datasource, decl) {
   };
 };
 
-function ramp (datasource, column, method, min, max) {
-  if (!Array.isArray(min)) {
-    return numericRamp(datasource, column, method, min, max);
+/**
+ * @param datasource
+ * @param {String} column
+ * @param args
+ *
+ * ####################
+ * ###  COLOR RAMP  ###
+ * ####################
+ *
+ * [colorbrewer(Greens, 7), jenks]
+ *  <Array>scheme         , <String>method
+ *
+ * [colorbrewer(Greens, 7)]
+ *  <Array>scheme
+ *
+ * ####################
+ * ### NUMERIC RAMP ###
+ * ####################
+ *
+ * [10            , 20]
+ *  <Number>minVal, <Number>maxValue
+ *
+ * [10            , 20,             , jenks]
+ *  <Number>minVal, <Number>maxValue, <String>method
+ *
+ * [10            , 20,             , 4]
+ *  <Number>minVal, <Number>maxValue, <Number>buckets
+ *
+ * [10            , 20,             , 4              , jenks]
+ *  <Number>minVal, <Number>maxValue, <Number>buckets, <String>method
+ */
+function ramp (datasource, column, args) {
+  var method;
+
+  if (Array.isArray(args[0])) {
+    var scheme = args[0];
+    method = args[1];
+
+    return colorRamp(datasource, column, scheme, method);
   }
 
-  var scheme = min;
-  return colorRamp(datasource, column, method, scheme);
+  var min = +args[0];
+  var max = +args[1];
+
+  var buckets = 5;
+  method = args[2];
+
+  if (Number.isFinite(+args[2])) {
+    buckets = +args[2];
+    method = args[3];
+  }
+
+  return numericRamp(datasource, column, min, max, buckets, method);
 }
 
-function getRamp (datasource, column, method) {
+function getRamp (datasource, column, buckets, method) {
   return new Promise(function (resolve, reject) {
-    datasource.getRamp(columnName(column), method, function (err, ramp) {
+    datasource.getRamp(columnName(column), buckets, method, function (err, ramp) {
       if (err) {
         return reject(err);
       }
@@ -58,15 +104,16 @@ function getRamp (datasource, column, method) {
   });
 }
 
-function colorRamp (datasource, column, method, scheme) {
-  return getRamp(datasource, column, method)
+function colorRamp (datasource, column, scheme, method) {
+  var buckets = scheme.length;
+  return getRamp(datasource, column, buckets, method)
     .then(function (ramp) {
       var i;
       var rampResult = [];
 
       rampResult.push(scheme[0]);
 
-      for (i = 0; i < 5; i++) {
+      for (i = 0; i < buckets; i++) {
         rampResult.push(ramp[i]);
         rampResult.push(scheme[i]);
       }
@@ -75,8 +122,8 @@ function colorRamp (datasource, column, method, scheme) {
     });
 }
 
-function numericRamp (datasource, column, method, min, max) {
-  return getRamp(datasource, column, method)
+function numericRamp (datasource, column, min, max, buckets, method) {
+  return getRamp(datasource, column, buckets, method)
     .then(function (ramp) {
       var i;
       var rampResult = [];
@@ -84,9 +131,9 @@ function numericRamp (datasource, column, method, min, max) {
       min = +min;
       max = +max;
       var range = max - min;
-      var width = range / 5;
+      var width = range / buckets;
       rampResult.push(min);
-      for (i = 0; i < 5; i++) {
+      for (i = 0; i < buckets; i++) {
         rampResult.push(ramp[i]);
         rampResult.push(min + ((i + 1) * width));
       }
--- test/support/dummy-datasource.js
+++ test/support/dummy-datasource.js
@@ -9,6 +9,6 @@ DummyDatasource.prototype.getName = function () {
   return 'DummyDatasource';
 };
 
-DummyDatasource.prototype.getRamp = function (column, scheme, callback) {
+DummyDatasource.prototype.getRamp = function (column, buckets, method, callback) {
   return callback(null, [100000, 250000, 500000, 1e6, 1.5e6, 2e6, 1e7]);
 };
