3f29722e0f211f3420e8586aca3771e5848e5a6b
Francisco Dans <fdansv@gmail.com>
2016-05-04 09:56:20 +0200

    cartocolor function

---
+++ src/fn/fn-cartocolor.js
@@ -0,0 +1,23 @@
+'use strict';
+
+require('es6-promise').polyfill();
+
+var cartocolor = require('cartocolor');
+var debug = require('../helper/debug')('fn-factory');
+
+module.exports = function () {
+  return function fn$cartocolor (scheme, numberDataClasses) {
+    debug('fn$cartocolor(%j)', arguments);
+    numberDataClasses = Math.min(7, Math.max(3, numberDataClasses || 5));
+    return new Promise(function (resolve) {
+      var result = cartocolor.BluPurp1[numberDataClasses];
+      var def = cartocolor[scheme];
+      if (def) {
+        result = def[numberDataClasses];
+      }
+      resolve(result);
+    });
+  };
+};
+
+module.exports.fnName = 'cartocolor';
