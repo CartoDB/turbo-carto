a7bc7ece58c03bbcc0bcb97b9f3434ac9be3e96a
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-07-19 19:52:54 +0200

    Add test for less_than strategy

--- test/acceptance/ramp-range.test.js
+++ test/acceptance/ramp-range.test.js
@@ -5,15 +5,15 @@ var turbocarto = require('../../src/index');
 var DummyDatasource = require('../support/dummy-datasource');
 
 describe('range issue', function () {
-  it('should generate a valid cartocss for a ramp with only one bucket/value', function (done) {
+  it('should generate a valid cartocss for a ramp with only one bucket/value and ">" strategy', function (done) {
     var turboCartocss = [
       '#layer{',
-      '  marker-width: ramp([cartodb_id], cartocolor(SunsetDark, 10), jenks);',
+      '  marker-fill: ramp([cartodb_id], cartocolor(SunsetDark, 10), jenks);',
       '}'
     ].join('\n');
     var expectedCartocss = [
       '#layer{',
-      '  marker-width: #fcde9c;',
+      '  marker-fill: #fcde9c;',
       '}'
     ].join('\n');
     var datasource = new DummyDatasource(function () {
@@ -22,7 +22,34 @@ describe('range issue', function () {
         stats: { min_val: 5, max_val: 5, avg_val: 5 }
       };
     });
-    turbocarto(turboCartocss, datasource, function (err, result, metadata) {
+    turbocarto(turboCartocss, datasource, function (err, result) {
+      if (err) {
+        return done(err);
+      }
+
+      assert.equal(result, expectedCartocss);
+      done();
+    });
+  });
+
+  it('should generate a valid cartocss for a ramp with only one bucket/value and "<" strategy', function (done) {
+    var turboCartocss = [
+      '#layer{',
+      '  marker-fill: ramp([cartodb_id], cartocolor(SunsetDark, 10), headtails(1));',
+      '}'
+    ].join('\n');
+    var expectedCartocss = [
+      '#layer{',
+      '  marker-fill: #7c1d6f;',
+      '}'
+    ].join('\n');
+    var datasource = new DummyDatasource(function () {
+      return {
+        ramp: [1],
+        stats: { min_val: 1, max_val: 1, avg_val: 1 }
+      };
+    });
+    turbocarto(turboCartocss, datasource, function (err, result) {
       if (err) {
         return done(err);
       }
