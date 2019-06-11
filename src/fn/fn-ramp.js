ca41ff919421303b910900b1804b85eca54603fb
Rafa de la Torre <rtorre@carto.com>
2019-06-11 16:58:17 +0200

    Fix for LazyFiltersResult: differenciate promise and value
    Committing this on behalf of @dgaubert
    
    See https://github.com/CartoDB/turbo-carto/pull/84#issuecomment-500874187
    
    We need to differentiate when we have an instance of
    `LazyFiltersResult` or `ValuesResult` as they return completely
    different things, a promise or a value.

--- src/fn/fn-ramp.js
+++ src/fn/fn-ramp.js
@@ -188,8 +188,22 @@ function strategyFromMapping (mapping) {
 function compatibilityValuesRamp (datasource, column, args) {
   var values = args[0];
   var method = (args[1] || 'quantiles').toLowerCase();
-  var numBuckets = values.getLength();
-  return getRamp(datasource, column, numBuckets, method).then(compatibilityCreateRampFn(values));
+
+  if (values.is(LazyFiltersResult)) {
+    var mapping = args[2];
+    var strategy = strategyFromMapping(mapping);
+
+    return values.getLength(column, strategy)
+      .then(function (numBuckets) {
+        return getRamp(datasource, column, numBuckets, method);
+      })
+      .then(compatibilityCreateRampFn(values));
+  } else {
+    var numBuckets = values.getLength();
+
+    return getRamp(datasource, column, numBuckets, method)
+      .then(compatibilityCreateRampFn(values));
+  }
 }
 
 /**
